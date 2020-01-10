447fef7fc863c76db42afd1a1f1533e365b778bd
Hiroyuki Tanjo <expheno@gmail.com>
2020-01-10 15:34:34 +0900

    Send request when click a button

--- Controller.js
+++ Controller.js
@@ -6,6 +6,7 @@ import ButtonStacked from './ButtonStacked';
 import ButtonCombined from './ButtonCombined';
 import { gutterSize } from './styles';
 
+const apiUrl = 'https://jsonplaceholder.typicode.com/posts';
 const sendInterval = 200;   // milliseconds
 const styles = StyleSheet.create({
   controller: {
@@ -36,13 +37,31 @@ const styles = StyleSheet.create({
   },
 });
 
+async function sendCommand(command) {
+  const method = 'POST';
+  const headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  };
+  const payload = { command };
+  const body = JSON.stringify(payload);
+  try {
+    const response = await fetch(apiUrl, { method, headers, body });
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 export default function Controller() {
   const [sending, setSending] = useState(false);
-  const handlePress = useCallback(() => {
+  const handlePress = useCallback(async command => {
     if (sending) return;
     setSending(true);
     setTimeout(() => setSending(false), sendInterval);
     Vibration.vibrate(60);
+    await sendCommand(command);
   }, [sending]);
   return (
     <View style={styles.controller}>
@@ -51,18 +70,30 @@ export default function Controller() {
       </View>
       <View style={styles.buttons}>
         <View style={styles.row}>
-          <ButtonStacked buttons={[{ icon: 'ios-arrow-up', onPress: handlePress }, { icon: 'ios-arrow-down', onPress: handlePress }]} />
-          <Button icon="md-tv" onPress={handlePress} />
-          <ButtonStacked buttons={[{ icon: 'md-add', onPress: handlePress }, { icon: 'md-remove', onPress: handlePress }]} />
+          <ButtonStacked buttons={[
+            { icon: 'ios-arrow-up', onPress: () => handlePress('tvChannelUp') },
+            { icon: 'ios-arrow-down', onPress: () => handlePress('tvChannelDown') }
+          ]} />
+          <Button icon="md-tv" onPress={() => handlePress('tvPowerToggle')} />
+          <ButtonStacked buttons={[
+            { icon: 'md-add', onPress: () => handlePress('tvVolumeUp') },
+            { icon: 'md-remove', onPress: () => handlePress('tvVolumeDown') }
+          ]} />
         </View>
         <View style={styles.row}>
-          <Button icon="ios-snow" onPress={handlePress} />
-          <Button icon="ios-flame" onPress={handlePress} />
-          <Button text="OFF" onPress={handlePress} />
+          <Button icon="ios-snow" onPress={() => handlePress('acCoolingOn')} />
+          <Button icon="ios-flame" onPress={() => handlePress('acHeatingOn')} />
+          <Button text="OFF" onPress={() => handlePress('acOff')} />
         </View>
         <View style={styles.row}>
-          <ButtonCombined buttons={[{ text: 'OFF', onPress: handlePress }, { icon: 'md-sunny', onPress: handlePress }]} />
-          <ButtonCombined buttons={[{ text: 'OFF', onPress: handlePress }, { icon: 'md-sunny', onPress: handlePress }]} />
+          <ButtonCombined buttons={[
+            { text: 'OFF', onPress: () => handlePress('livingLightOff') },
+            { icon: 'md-sunny', onPress: () => handlePress('livingLightOn') }
+          ]} />
+          <ButtonCombined buttons={[
+            { text: 'OFF', onPress: () => handlePress('bedroomLightOff') },
+            { icon: 'md-sunny', onPress: () => handlePress('bedroomLightOn') }
+          ]} />
         </View>
       </View>
       <View style={styles.spacer} />
